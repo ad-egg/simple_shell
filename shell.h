09c1bd303958a036271c7f270b5e0460db6edc63
shaadibo <colbertrashaad@gmail.com>
2018-11-14 19:38:10 +0000

    header

--- shell.h
+++ shell.h
@@ -11,7 +11,7 @@
 
 typedef struct path
 {
-	char *head;
+	char *ptr;
 	struct path *next;
 } path_t;
 
@@ -22,6 +22,9 @@ int main(int ac, char **av);
 void egg(char *snail);
 char *read_line(void);
 char **split_line(char *buffer, char *del);
+
+/* path functions */
+path_t path_gen(void);
 
 /* execution */
 int cmp_exec(char **args, char *eggs);
