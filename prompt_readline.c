66f23b7c37191a24506a6bf17e60892343ce0625
Athena Deng <539@holbertonschool.com>
2018-11-17 02:26:03 +0000

    removed newline character from end of input line

--- prompt_readline.c
+++ prompt_readline.c
@@ -9,8 +9,14 @@ char *prompt_getline(void)
 	char *buffer = NULL;
 	size_t buffer_size;
 	char *prompt = "$ ";
+	int i;
 
 	write(STDOUT_FILENO, prompt, 2);
 	getline(&buffer, &buffer_size, STDIN_FILENO);
+	for (i = 0; buffer[i] != '\0'; i++)
+	{
+		if (buffer[i - 1] == '\n')
+			buffer[i - 1] = '\0';
+	}
 	return (buffer);
 }
