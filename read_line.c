8dd73fb0fcff6923fe9c105bf6c55fa81785e484
Athena Deng <539@holbertonschool.com>
2018-11-20 02:13:34 +0000

    removed trailing whitespaces, added required space after comma and open parentheses

--- read_line.c
+++ read_line.c
@@ -1,5 +1,5 @@
 #include "shell.h"
-/*
+/**
  * read_line - reads the line
  * Return: 0
 */
@@ -7,13 +7,12 @@ char *read_line(void)
 {
 	char *line = NULL;
 	size_t bufsize = 0; /*will change according to input*/
-	
-	
+
 	write(1, "$ ", 2);
-	if(getline(&line, &bufsize, stdin) <= 0)
+	if (getline(&line, &bufsize, stdin) <= 0)
 	{
-		if(isatty(STDIN_FILENO) == 1)
-			write(STDOUT_FILENO, "\n",1);
+		if (isatty(STDIN_FILENO) == 1)
+			write(STDOUT_FILENO, "\n", 1);
 		shell_exit(NULL, line);
 	}
 	return (line);
